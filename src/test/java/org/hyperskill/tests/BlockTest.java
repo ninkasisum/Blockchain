7652e66ffe859bf7fa8f18efd8cefae25e0bd6a1
Diogo Junqueira Geraldo <diogojunqueirageraldo@gmail.com>
2020-06-24 10:26:17 -0300

    add: Test case with junit 4

---
+++ src/test/java/org/hyperskill/tests/BlockTest.java
@@ -0,0 +1,50 @@
+package org.hyperskill.tests;
+
+import org.hyperskill.models.Block;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class BlockTest {
+    @Test
+    public void testeBlockCreationAndDataRetrieve()
+            throws NoSuchPaddingException,
+            UnsupportedEncodingException,
+            IllegalBlockSizeException,
+            BadPaddingException,
+            NoSuchAlgorithmException,
+            InvalidKeyException {
+
+        Block[] chain = new Block[100];
+
+        String prevHash = null;
+        for(int i = 0; i < chain.length; i++)
+        {
+            String data = String.format("Hello, I'am block %d", i);
+            String encryptionKey = String.format("encryptionKey%d", i);
+            Block block = new Block(data, prevHash, encryptionKey);
+            prevHash = block.getHash();
+            chain[i] = block;
+        }
+
+        for(int i = 0; i < chain.length; i++)
+        {
+            String expectedData = String.format("Hello, I'am block %d", i);
+            String encryptionKey = String.format("encryptionKey%d", i);
+            Block block = chain[i];
+            String retrievedData = block.getData(encryptionKey);
+
+            if (!expectedData.equals(retrievedData))
+                throw new AssertionError("There was an error while getting data from block");
+        }
+
+        Assume.assumeTrue("Data retrieved with success", true);
+    }
+}
